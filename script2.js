d550a16fbb55b0b9a98e1fb441644c817393270a
Irina <55109138+IrinaSKab@users.noreply.github.com>
2019-10-09 01:10:50 +0300

    test innerHTML

--- script2.js
+++ script2.js
@@ -1,30 +1,35 @@
 var form = document.getElementById("form");
 
+
+function fill(field) {	
+	var fieldValue = field && field.value;
+
+	if (fieldValue === ''){
+		field.style.borderBottom = "5px solid red";
+		document.getElementById("info").innerHTML = "Обратите внимание, что не все поля заявки заполнены.";
+	}
+	else {
+		field.style.borderBottom = "5px solid rgba(0, 0, 0, 0.5)";
+        document.getElementById("info").innerHTML =  "Спасибо, ваша заявка отправлена.";
+	}
+}
+
+
 function validate(event) {
 	event.preventDefault();
 	var myName = document.getElementById("name");
-	var myNameValue = myName && myName.value;
-
 	var myEmail = document.getElementById("email");
-	var myEmailValue = myEmail && myEmail.value;
-
 	var mySubject = document.getElementById("subject");
-	var mySubjectValue = mySubject && mySubject.value;
-
 	var myMessage = document.getElementById("message");
-	var myMessageValue = myMessage && myMessage.value;  
 
-	function fill() {	
-		if ((myNameValue === '') & (myEmailValue === '') & (mySubjectValue === '') & (myMessageValue === '')) {
-			(myName.style.border= "5px solid red") & (Bottom&myName.style.borderBottom = "5px solid red") & (mySubject.style.borderBottom= "5px solid red") & (myMessage.style.borderBottom = "5px solid red");
-		}
-		else {
-			(myName.style.borderBottom = "5px solid rgba(0, 0, 0, 0.5)") & (myName.style.borderBottom = "5px solid rgba(0, 0, 0, 0.5)") & (mySubject.style.borderBottom = "5px solid rgba(0, 0, 0, 0.5)") & (myMessage.style.borderBottom = "5px solid rgba(0, 0, 0, 0.5)");   
-		}
-	}
-	form.addEventListener("submit", fill);
-}	
+	fill(myName);
+	fill(myEmail);
+	fill(mySubject);
+	fill(myMessage);
+}
+
 form.addEventListener("submit", validate);
+
 
 
 
