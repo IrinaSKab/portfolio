a4e0e0da084c60a0c81f2c2bd3b4c9229e158807
Irina <55109138+IrinaSKab@users.noreply.github.com>
2019-10-08 07:15:35 +0300

    test script

--- script2.js
+++ script2.js
@@ -1,7 +1,33 @@
 var form = document.getElementById("form");
 
-function validate() {
-	console.log("test");
-}
+function validate(event) {
+	event.preventDefault();
+	var myName = document.getElementById("name");
+	var myNameValue = myName && myName.value;
 
+	var myEmail = document.getElementById("email");
+	var myEmailValue = myEmail && myEmail.value;
+
+	var mySubject = document.getElementById("subject");
+	var mySubjectValue = mySubject && mySubject.value;
+
+	var myMessage = document.getElementById("message");
+	var myMessageValue = myMessage && myMessage.value;  
+
+	function fill() {	
+		if ((myNameValue === '') & (myEmailValue === '') & (mySubjectValue === '') & (myMessageValue === '')) {
+			(myName.style.border= "5px solid red") & (Bottom&myName.style.borderBottom = "5px solid red") & (mySubject.style.borderBottom= "5px solid red") & (myMessage.style.borderBottom = "5px solid red");
+		}
+		else {
+			(myName.style.borderBottom = "5px solid rgba(0, 0, 0, 0.5)") & (myName.style.borderBottom = "5px solid rgba(0, 0, 0, 0.5)") & (mySubject.style.borderBottom = "5px solid rgba(0, 0, 0, 0.5)") & (myMessage.style.borderBottom = "5px solid rgba(0, 0, 0, 0.5)");   
+		}
+	}
+	form.addEventListener("submit", fill);
+}	
 form.addEventListener("submit", validate);
+
+
+
+
+
+
